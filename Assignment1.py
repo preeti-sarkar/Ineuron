5076066a27fe96de88d5585aca1fb6f5a7b89170
preeti-sarkar <75533224+preeti-sarkar@users.noreply.github.com>
2020-12-19 22:04:14 +0530

    Add files via upload
    assignment

---
+++ Assignment1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+lst=[]
+for i in range(2000,3200):
+    if i%7==0 and i%5!=0:
+        lst.append(i)
+print(lst)
+
+
+# In[2]:
+
+
+a=input("What is your first name?")
+b=input("What is your last name?")
+print(a[::-1], b[::-1])
+
+
+# In[3]:
+
+
+import math
+v= math.pi*4*(6**3)/3
+print(v)
+
+
+# In[ ]:
+
+
+
+
